049f894067b90cb8e193ae361720b4e6a4eaf5a8
Lalit Tanwar <lalit.tanwar07@gmail.com>
2015-04-05 19:19:12 +0530

    added more functions

--- polyfills.js
+++ polyfills.js
@@ -34,3 +34,14 @@ if (typeof String.prototype.endsWith !== 'function') {
 		return this.indexOf(suffix, this.length - suffix.length) !== -1;
 	};
 }
+
+if(typeof CustomEvent !== "function"){
+	function CustomEvent ( event, params ) {
+		   params = params || { bubbles: false, cancelable: false, detail: undefined };
+		   var evt = document.createEvent( 'CustomEvent' );
+		   evt.initCustomEvent( event, params.bubbles, params.cancelable, params.detail );
+		   return evt;
+	};
+	CustomEvent.prototype = window.Event.prototype;
+	window.CustomEvent = CustomEvent;	
+}
