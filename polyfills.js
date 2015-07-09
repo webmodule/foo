4fa3307dc1c55a655eda38210ec7326317ff566a
Lalit Tanwar <lalit.tanwar07@gmail.com>
2015-07-09 22:47:38 +0530

    Function ordering

--- polyfills.js
+++ polyfills.js
@@ -1,73 +1,84 @@
-if(!window.BrowserDetect){
-	window.BrowserDetect = new (function(){
-		this.isBelowIE9 = false;
-	});
-}
-
-if (typeof Object.getPrototypeOf !== "function") {
-	if (typeof "test".__proto__ === "object") {
-		Object.getPrototypeOf = function(object) {
-			return object.__proto__;
-		};
-	} else {
-		Object.getPrototypeOf = function(object) {
-			return object.constructor.prototype;
-		};
+// Custom Event
+(function(foo){
+	
+	if (typeof foo.CustomEvent !== "function") {
+		var x = function CustomEvent(event, params) {
+			params = params || {
+				bubbles : false,
+				cancelable : false,
+				detail : undefined
+			};
+			var evt = document.createEvent('CustomEvent');
+			evt.initCustomEvent(event, params.bubbles, params.cancelable,
+					params.detail);
+			return evt;
+		}
+		;
+		CustomEvent.prototype = foo.Event.prototype;
+		foo.CustomEvent = x;
 	}
-}
-if (typeof Object.setPrototypeOf !== "function") {
-	if (typeof "test".__proto__ === "object") {
-		if(false && typeof Object.defineProperty=='function'){
-			Object.setPrototypeOf = function(object, proto) {
-				Object.defineProperty(object, 'prototype', {
-					  value : proto
-				});
-				return proto;
+
+	if (!foo.BrowserDetect) {
+		foo.BrowserDetect = new (function() {
+			this.isBelowIE9 = false;
+		});
+	}
+
+	if (typeof Object.getPrototypeOf !== "function") {
+		if (typeof "test".__proto__ === "object") {
+			Object.getPrototypeOf = function(object) {
+				return object.__proto__;
 			};
 		} else {
-			Object.setPrototypeOf = function(object, proto) {
-				return object.__proto__ = proto;
+			Object.getPrototypeOf = function(object) {
+				return object.constructor.prototype;
 			};
 		}
-	} else {
-		Object.setPrototypeOf = function(object, proto) {
-			return object.constructor.prototype = proto;
+	}
+	if (typeof Object.setPrototypeOf !== "function") {
+		if (typeof "test".__proto__ === "object") {
+			if (false && typeof Object.defineProperty == 'function') {
+				Object.setPrototypeOf = function(object, proto) {
+					Object.defineProperty(object, 'prototype', {
+						value : proto
+					});
+					return proto;
+				};
+			} else {
+				Object.setPrototypeOf = function(object, proto) {
+					return object.__proto__ = proto;
+				};
+			}
+		} else {
+			Object.setPrototypeOf = function(object, proto) {
+				return object.constructor.prototype = proto;
+			};
+		}
+	}
+	if (typeof String.prototype.endsWith !== 'function') {
+		String.prototype.endsWith = function(suffix) {
+			return this.indexOf(suffix, this.length - suffix.length) !== -1;
 		};
 	}
-}
-if (typeof String.prototype.endsWith !== 'function') {
-	String.prototype.endsWith = function(suffix) {
-		return this.indexOf(suffix, this.length - suffix.length) !== -1;
-	};
-}
 
-//Array Pollyfills
-if(typeof Array.prototype.unique !== 'function'){
-	Array.prototype.unique = function(cb){
-		if(typeof cb !== 'function'){
-			   var u = {}, a = [];
-			   for(var i = 0, l = this.length; i < l; ++i){
-			      if(u.hasOwnProperty(this[i])) {
-			         continue;
-			      }
-			      a.push(this[i]);
-			      u[this[i]] = 1;
-			   }
-			   return a;
-		} else {
-			
-		}
-	};
-}
+	// Array Pollyfills
+	if (typeof Array.prototype.unique !== 'function') {
+		Object.defineProperties(Array.prototype, {
+			unique : {
+				value : function() {
+					var u = {}, a = [];
+					for (var i = 0, l = this.length; i < l; ++i) {
+						if (u.hasOwnProperty(this[i])) {
+							continue;
+						}
+						a.push(this[i]);
+						u[this[i]] = 1;
+					}
+					return a;
+				}
+			}
+		});
+	}
 
-//Custom Event
-if(typeof CustomEvent !== "function"){
-	function CustomEvent ( event, params ) {
-		   params = params || { bubbles: false, cancelable: false, detail: undefined };
-		   var evt = document.createEvent( 'CustomEvent' );
-		   evt.initCustomEvent( event, params.bubbles, params.cancelable, params.detail );
-		   return evt;
-	};
-	CustomEvent.prototype = window.Event.prototype;
-	window.CustomEvent = CustomEvent;	
-}
+	
+})(this)
